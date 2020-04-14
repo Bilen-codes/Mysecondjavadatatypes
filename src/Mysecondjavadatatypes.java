205ef22dcc5a34b4c73ccdf599d5d8957f88a62e
Bilen Worku <bilenworku@Bilens-MacBook-Air.local>
2020-04-14 10:23:35 -0400

    firstcommit

---
+++ src/Mysecondjavadatatypes.java
@@ -0,0 +1,23 @@
+public class Mysecondjavadatatypes {
+    public static void main(String args[]) {
+    //declared the data types
+    boolean flag = true;
+    byte b = 4;
+    char a = 'A';
+    short index = 3;
+    int i = 89;
+    float f = 4.73f;
+    long longVar = 123456789;
+    double doubaleVar = 123456789.123456789;
+
+    //displays the data types
+                System.out.println("flag: "+flag);
+                System.out.println("byte: "+b);
+               System.out.println("char : "+a);
+               System.out.println("short: "+index);
+               System.out.println("integer: "+i);
+               System.out.println("float: "+f );
+               System.out.println("long: "+longVar);
+               System.out.println("double: "+doubaleVar);
+    }
+}
